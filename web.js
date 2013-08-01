a5f8b789745dd6287593b0a98e1d5bdd4bc238be
Alessio De Rossi <derox99@gmail.com>
2013-08-01 23:28:16 +0200

    mod web.js

--- web.js
+++ web.js
@@ -1,19 +1,19 @@
 
 var express = require('express');
-var app = express();
 var fs = require('fs');
+var app = express();
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', function(err, cont)
-  {
-	if(err) throw err;
-        response.send(cont.toString());
-   });
+fs.readFile('index.html', function(err, cont)
+{
+if(err) throw err;
+response.send(cont.toString());
+});
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
