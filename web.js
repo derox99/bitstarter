c742bc4c3ac8ff305742ab165e42ca8068238fe5
Alessio De Rossi <derox99@gmail.com>
2013-08-14 13:07:57 +0200

    web.js fro file

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 
 var express = require('express');
-
+var fs=require('fs');
 var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-    response.send('Hello world from Ale!');
+    fs.readFile('index.html',function(err,data){
+	if(err) throw err;
+	response.send(data.toString());
+    })
 });
 
 var port = process.env.PORT || 5000;
